1be7e7c9cd303a461a424148c73dd72ee3efcbea
Etienne0101 <etiennepichotdamon@gmail.com>
2023-08-02 10:16:39 +0200

    update viz

--- components/nav/DataViz.js
+++ components/nav/DataViz.js
@@ -4,11 +4,13 @@ import styles from '../../styles/DataViz.module.css';
 
 const DataViz = ({ title, description, children }) => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles['viz-title']}>{title}</h1>
-      <p className={styles['dataviz-description']}>{description}</p>
-      <div className={styles['dataviz-visualization']}>
-        {children}
+    <div className={styles['parent-container']}>
+      <div className={styles.container}>
+        <h1 className={styles['viz-title']}>{title}</h1>
+        <p className={styles['dataviz-description']}>{description}</p>
+        <div className={styles['dataviz-visualization']}>
+          {children}
+        </div>
       </div>
     </div>
   );
