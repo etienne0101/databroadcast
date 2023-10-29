0b26c9318129900788f52bcfb297b06416331af0
Etienne0101 <etiennepichotdamon@gmail.com>
2023-10-29 20:32:24 +0100

    update index

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,23 @@ import styles from '../styles/Home.module.css'
 const HomePage = () => {
   const galleryData = [
     {
-      title: 'Public art in canadian cities',
-      description: 'Maps of public art and year of installation',
-      imagePath: './images/datastories/canada-public-art.jpeg',
-      link: '/datastories/canada-public-art'
+      title: 'Lawmakers and Drug Statistics: The Art of Ignoring the Data',
+      description:
+        'Maps and charts about drug use VS policies across the globe',
+      imagePath: './images/datastories/drug-policies.jpeg',
+      link: '/datastories/drug-policies'
     },
     {
       title: 'Exploring the Global Terrorism DataBase',
       description: 'Maps and charts or terrorist attacks since 1970',
       imagePath: './images/datastories/global-terrorism.jpeg',
       link: '/datastories/global-terrorism'
+    },
+    {
+      title: 'Public art in canadian cities',
+      description: 'Maps of public art and year of installation',
+      imagePath: './images/datastories/canada-public-art.jpeg',
+      link: '/datastories/canada-public-art'
     }
   ]
 
